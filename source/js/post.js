b5ec774b2e02334ef402815d980d6cfd5b9762a9
NieLamu <mxmxlty@gmail.com>
2019-03-15 00:23:42 +0800

    Some changes of theme

--- source/js/post.js
+++ source/js/post.js
@@ -24,6 +24,18 @@ $(document).ready(function(){
     $(window).scroll(placeToc) 
     $(window).resize(placeToc)
 
+    tocbot.init({
+        tocSelector: '#tocbot',
+        contentSelector: '.post_content',
+        headingSelector: 'h1, h2, h3, h4, h5, h6',
+        linkClass: 'tocbot-link',
+        activeLinkClass: 'tocbot-active-link',
+        listClass: 'tocbot-list',
+        isCollapsedClass: 'tocbot-is-collapsed',
+        collapsibleClass: 'tocbot-is-collapsible',
+        scrollSmooth: true,
+    });
+
 
     $(".post_content img").on("click", function(e){
         $("body").append(
@@ -44,16 +56,48 @@ $(document).ready(function(){
         });
     });
 
-    tocbot.init({
-        tocSelector: '#tocbot',
-        contentSelector: '.post_content',
-        headingSelector: 'h1, h2, h3, h4, h5, h6',
-        linkClass: 'tocbot-link',
-        activeLinkClass: 'tocbot-active-link',
-        listClass: 'tocbot-list',
-        isCollapsedClass: 'tocbot-is-collapsed',
-        collapsibleClass: 'tocbot-is-collapsible',
-        scrollSmooth: true,
-    });
+    $(document).ready(()=> {
+        const canvas = $('#post_img_canvas')[0];
+        const imgSrc = $(".post_banner_img_container a").attr("href");
+        const img = new Image();
+        img.crossOrigin = 'Anonymous';
+        /**等图片资源加载完成后，才在 Canvas 上进行绘制渲染*/
+        img.onload = ()=> {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            const ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+            let pixels = [[], [], []];
+            console.log(data);
+            const len = data.length;
+            for (let i = 0, offset, r, g, b, a; i < len/4; i++) {
+                offset = i * 4;
+                r = data[offset + 0];
+                g = data[offset + 1];
+                b = data[offset + 2];
+                a = data[offset + 3];
+                // If pixel is mostly opaque and not white
+                if (a >= 125) {
+                    if (!(r > 250 && g > 250 && b > 250)) {
+                        pixels[0].push(r);
+                        pixels[1].push(g);
+                        pixels[2].push(b);
+                    }
+                }
+            }
+            for (let i = 0; i < 3; i++) {
+                pixels[i] = parseInt(eval(pixels[i].join('+'))/pixels[i].length);
+            }
+            const color = `rgb(${pixels[0]}, ${pixels[1]}, ${pixels[2]})`;
+            console.log(color);
+            $('.page-header').css("background-color", color)
+
+        }
+        img.src = imgSrc;
+
+    
+
+    })
 
 }) 
